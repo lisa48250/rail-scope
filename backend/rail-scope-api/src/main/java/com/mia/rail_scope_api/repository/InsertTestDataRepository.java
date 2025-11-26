8eb21cd8be3b63d529b9a2dcb5ac89ebc6c46c5c
mimi <lisa48250@gmail.com>
2025-11-26 18:18:21 +0800

    fix: 整理程式

--- backend/rail-scope-api/src/main/java/com/mia/rail_scope_api/repository/InsertTestDataRepository.java
+++ backend/rail-scope-api/src/main/java/com/mia/rail_scope_api/repository/InsertTestDataRepository.java
@@ -1,6 +1,5 @@
 package com.mia.rail_scope_api.repository;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Repository;
 
 import com.mia.rail_scope_api.mapper.InsertTestDataMapper;
